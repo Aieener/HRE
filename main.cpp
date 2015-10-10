2987a8d88976e475a860d7e31dd3d567b414c2e1
Aieener <aikurt@hotmail.com>
2015-10-10 19:20:23 -0400

    fix main.cpp to runing record time

--- main.cpp
+++ main.cpp
@@ -20,6 +20,11 @@ int main()
 	// m.Zvs_();
 	// ======================= end of simulation, print out the time =======
 	double end = clock();
-	cout <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC)<<endl;
+	cout <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC)<<" sec."<<endl;
+	t <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC) <<" sec."<<endl;
+	ofstream myfile ("time.txt");
+	string datat = t.str();
+	myfile << datat;
+	myfile.close();	
 	return 0;
 }
